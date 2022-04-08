98e7a5c24ef43794a2bd800f6ec99e714bc64f78
tariktesfa <tariktesfa9090@gmail.com>
2022-04-08 09:37:28 +0300

    Exercise on Variable, If else and while loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
