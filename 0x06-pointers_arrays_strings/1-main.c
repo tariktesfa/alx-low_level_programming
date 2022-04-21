7df4ec256dcb5b2667a672d09b0dcefef7431b1f
tariktesfa <tariktesfa9090@gmail.com>
2022-04-21 14:08:33 +0300

    Advanced pointers, arrays and strings Exercises

---
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <unistd.h>
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,31 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+char *_strcat(char *dest, char *src);
+
+void print_number(int n);
+
+int _putchar(char c);
+
+void print_buffer(char *b, int size);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
+
+char *rot13(char *);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+#endif
+
