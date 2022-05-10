e97b232f90e1625fa5fff33b1bd56e6ecb34b780
tariktesfa <tariktesfa9090@gmail.com>
2022-05-11 00:07:46 +0300

    Preprocessor Excercises

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h
+ * Auth: Brennan D Baraban
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+#define PI 3.14159265359
+
+#endif
+
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+#ifndef OBJECT_LIKE
